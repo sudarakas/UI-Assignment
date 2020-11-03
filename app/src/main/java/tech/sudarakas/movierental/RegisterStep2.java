77a4e9d1e759c16aa4c9cccac4faa09fa3f535d0
sudarakas <sudharakafb@gmail.com>
2020-11-04 02:08:16 +0530

    init file upload with setp 2 completed

---
+++ app/src/main/java/tech/sudarakas/movierental/RegisterStep2.java
@@ -0,0 +1,47 @@
+package tech.sudarakas.movierental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class RegisterStep2 extends AppCompatActivity {
+
+    private Button btnNextTwo;
+    private Spinner spinnerObject;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_setp_2);
+
+        spinnerObject = (Spinner) findViewById(R.id.genderSpinner);
+        spinnerObject.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                ((TextView)parentView.getChildAt(0)).setTextColor(Color.WHITE);
+                Log.d("myTag", "This is my message");
+            }
+        });
+
+        btnNextTwo = (Button) findViewById(R.id.btnNextTwo);
+
+        btnNextTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(), RegisterStep3.class);
+                startActivity(next);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/tech/sudarakas/movierental/RegisterStep3.java
@@ -0,0 +1,14 @@
+package tech.sudarakas.movierental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterStep3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_setp_2);
+    }
+}
